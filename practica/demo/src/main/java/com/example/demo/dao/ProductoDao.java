7f2da61f00257bc2734a6deae79ff6a70f8d91d6
ryusdar <134015274+ryusdar@users.noreply.github.com>
2025-09-22 17:26:00 -0300

    Add files via upload

---
+++ practica/demo/src/main/java/com/example/demo/dao/ProductoDao.java
@@ -0,0 +1,11 @@
+package com.example.demo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Producto;
+
+@Repository
+public interface ProductoDao extends JpaRepository<Producto, Long>  {
+
+}
---
+++ practica/demo/src/main/java/com/example/demo/model/Producto.java
@@ -0,0 +1,80 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "id_producto")
+    private Long id_producto;
+    @Column(name = "stock")
+
+    private int stock;
+
+    public Producto(Long id, String nombre, Long id_producto, int stock) {
+        this.id = id;
+        this.nombre = nombre;
+        this.id_producto = id_producto;
+        this.stock = stock;
+    }
+
+    public Producto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Long getId_producto() {
+        return id_producto;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void setId_producto(Long id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Producto{");
+        sb.append("id=").append(id);
+        sb.append(", nombre=").append(nombre);
+        sb.append(", id_producto=").append(id_producto);
+        sb.append('}');
+        return sb.toString();
+    }
+
+}
