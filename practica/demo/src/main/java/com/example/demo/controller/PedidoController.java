d1ca6f0252d059806b5edd2828e6cbc3f0d2f649
ryusdar <134015274+ryusdar@users.noreply.github.com>
2025-09-22 19:35:45 -0300

    modificacion en controladores

--- practica/demo/src/main/java/com/example/demo/controller/PedidoController.java
+++ practica/demo/src/main/java/com/example/demo/controller/PedidoController.java
@@ -1,5 +1,35 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dao.PedidoDao;
+import com.example.demo.model.Pedido;
+
+@RestController
 public class PedidoController {
-    
+      @Autowired
+    private PedidoDao pedidoDao;
+
+    @GetMapping("api/producto")
+    public List<Pedido> getpedido() {
+        return pedidoDao.findAll();
+    }
+
+    @PostMapping("api/producto")
+    public void registrar(@RequestBody Pedido pedido) {
+       pedidoDao.save(pedido);
+    }
+
+    @DeleteMapping("api/producto/{id}")
+    public void eliminar(@PathVariable Long id) {
+        pedidoDao.deleteById(id);
+    }
 }
--- practica/demo/src/main/java/com/example/demo/controller/RevendedorController.java
+++ practica/demo/src/main/java/com/example/demo/controller/RevendedorController.java
@@ -1,5 +1,35 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.example.demo.dao.RevendedorDao;
+import com.example.demo.model.Revendedor;
+@RestController
 public class RevendedorController {
-    
+      @Autowired
+    private RevendedorDao revendedorDao;
+
+    @GetMapping("api/producto")
+    public List<Revendedor> getProducto() {
+        return revendedorDao.findAll();
+    }
+
+    @PostMapping("api/producto")
+    public void registrar(@RequestBody Revendedor revendedor) {
+        revendedorDao.save(revendedor);
+    }
+
+    @DeleteMapping("api/producto/{id}")
+    public void eliminar(@PathVariable Long id) {
+        revendedorDao.deleteById(id);
+    }
 }
