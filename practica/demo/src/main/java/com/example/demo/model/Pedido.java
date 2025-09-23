6a8e84efd492100b72d8918a00474ff23e03d651
ryusdar <134015274+ryusdar@users.noreply.github.com>
2025-09-22 22:15:19 -0300

    entidad pedido creado

--- practica/demo/src/main/java/com/example/demo/model/Pedido.java
+++ practica/demo/src/main/java/com/example/demo/model/Pedido.java
@@ -1,5 +1,98 @@
 package com.example.demo.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pedidos")
 public class Pedido {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "nro_pedido")
+    private Long nroPedido;
+
+    @Column(name = "fecha")
+    private LocalDate fecha;
+
+    @Column(name = "cantidad")
+    private int cantidad_productos;
+
+    @Column(name = "id_producto")
+    private Long idProducto;
+
+    @Column(name = "id_revendedor")
+    private Long idRevendedor;
+
+    // Constructor con parámetros
+    public Pedido(Long nroPedido, LocalDate fecha, int cantidad, Long idProducto, Long idRevendedor) {
+        this.nroPedido = nroPedido;
+        this.fecha = fecha;
+        this.cantidad_productos = cantidad;
+        this.idProducto = idProducto;
+        this.idRevendedor = idRevendedor;
+    }
+
+    // Constructor vacío (obligatorio para JPA)
+    public Pedido() {
+    }
+
+    // Getters y setters
+    public Long getNroPedido() {
+        return nroPedido;
+    }
+
+    public void setNroPedido(Long nroPedido) {
+        this.nroPedido = nroPedido;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+     public int getCantidad_productos() {
+        return cantidad_productos;
+    }
+
+    public void setCantidad_productos(int cantidad_productos) {
+        this.cantidad_productos = cantidad_productos;
+    }
+
+    public Long getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Long idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public Long getIdRevendedor() {
+        return idRevendedor;
+    }
+
+    public void setIdRevendedor(Long idRevendedor) {
+        this.idRevendedor = idRevendedor;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "nroPedido=" + nroPedido +
+                ", fecha=" + fecha +
+                ", cantidad=" + cantidad_productos +
+                ", idProducto=" + idProducto +
+                ", idRevendedor=" + idRevendedor +
+                '}';
+    }
+
 }
