bd3879b490e5a7970ae4b2b011d973db0634138d
Joaquin <joaquinruizperez2002@gmail.com>
2025-10-21 21:09:40 -0300

    Errores corregidos

--- practica/demo/target/classes/static/src/js/scripts.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('DOMContentLoaded', event => {
-    const listHoursArray = document.body.querySelectorAll('.list-hours li');
-    const todayIndex = new Date().getDay();
-
-    if (listHoursArray[todayIndex]) {
-        listHoursArray[todayIndex].classList.add('today');
-    }
-});
