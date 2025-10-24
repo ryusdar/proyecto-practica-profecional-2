808eebd8f8c301772260e1eac5304579ed5299ac
ryusdar <134015274+ryusdar@users.noreply.github.com>
2025-10-24 17:54:46 -0300

    b

--- practica/demo/src/main/java/com/example/demo/model/Localidad.java
+++ practica/demo/src/main/java/com/example/demo/model/Localidad.java
@@ -1,10 +1,19 @@
 package com.example.demo.model;
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-import jakarta.persistence.*;
-import java.util.List;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "localidad")
